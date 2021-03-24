cf61906084d68c0094b4b06ae703287340ec1521
Chansik Yun <chansigi@gmail.com>
2021-03-24 23:39:31 +0000

    .

--- 5_GoogleTest.cpp
+++ 5_GoogleTest.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <string>
 
+// 1.
 // ASSERT_XX: 단언문이 실패할 경우, 이후의 코드는 수행되지 않는다.
 //  => 죽은 단언문의 문제가 발생합니다.
 //  => 하나의 테스트 케이스 안에 여러 개의 단언문을 사용하는 것이 좋지 않다.
@@ -10,9 +11,8 @@
 // EXPECT_XX: 단언문이 실패하더라도, 이후의 코드는 계속 수행됩니다.
 //            하나의 단언문이 실패하면, 테스트의 결과는 실패가 됩니다.
 
-
 // ASSERT_EQ / TRUE / FALSE / LT / GT / LE / GE
-// EXPECT_EQ / TRUE / FALSE / LT / GT / LE / GE
+// EXPECT_EQ(==) / TRUE / FALSE / LT / GT / LE / GE
 TEST(GoogleTest, Sample1) {
 	int expected = 42;
 
@@ -25,3 +25,38 @@ TEST(GoogleTest, Sample1) {
 	EXPECT_EQ(expected, actual1) << "Reason 1";
 	EXPECT_EQ(expected, actual2) << "Reason 2";
 }
+
+
+// 2.
+//  C-Style 문자열: const char* - strcmp
+//   : EXPECT_STREQ / STRNE
+//     EXPECT_STRCASEEQ / STRCASENE
+
+//  C++ 문자열: std::string
+TEST(GoogleTest, Sample2) {
+	std::string s1 = "hello";
+	std::string s2 = "hello";
+
+	EXPECT_EQ(s1, s2);
+
+	const char* s3 = "HELLo";
+	const char* s4 = s1.c_str();
+
+	// EXPECT_EQ(s3, s4);
+	// EXPECT_STREQ(s3, s4);
+	EXPECT_STRCASEEQ(s3, s4);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
