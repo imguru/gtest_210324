a0b89d1beba443b698af5a1e2ede0f2e6249d9e0
Chansik Yun <chansigi@gmail.com>
2021-03-24 05:47:54 +0000

    .

--- 2_테스트픽스쳐3.cpp
+++ 2_테스트픽스쳐3.cpp
@@ -12,18 +12,29 @@ public:
 	// Calculator(int n) {}
 };
 
-// 픽스쳐 설치 방법
-//  3. 
-
-
-#include <gtest/gtest.h>
-
 #define SPEC printf
 
-TEST(CalculatorTest, Plus_2Plus2_Displays4) {
-	SPEC("2 더하기 2를 하였을 때 4가 나오는지 검증한다.\n");
-	Calculator* calc = new Calculator;
+// 픽스쳐 설치 방법
+//  3. Implicit Setup(암묵적 설치)
+//   : 여러 테스트에서 같은 테스트 픽스쳐의 코드를 SetUp() 함수에서 생성한다.
+//   => xUnit Test Framework 기능
+//   장점: 테스트 코드 중복을 제거하고, 꼭 필요하지 않은 상호작용(설치) 캡슐화할 수 있다.
+//   단점: 픽스쳐 설치 코드가 테스트 함수 밖에 존재하기 때문에, 테스트 함수만으로 테스트 코드를 이해하기 어려울 수 있다.
+//
+#include <gtest/gtest.h>
 
+class CalculatorTest : public testing::Test {
+protected:
+	Calculator* calc;
+
+	void SetUp() override {
+		printf("SetUp()\n");
+		calc = new Calculator;
+	}
+};
+
+TEST_F(CalculatorTest, Plus_2Plus2_Displays4) {
+	SPEC("2 더하기 2를 하였을 때 4가 나오는지 검증한다.\n");
 	calc->Enter(2);
 	calc->PressPlus();
 	calc->Enter(2);
@@ -32,17 +43,13 @@ TEST(CalculatorTest, Plus_2Plus2_Displays4) {
 	ASSERT_EQ(calc->Display(), 4) << "2+2 하였을 때";
 }
 
-TEST(CalculatorTest, PlusTest) {
-	Calculator* calc = new Calculator;
-
+TEST_F(CalculatorTest, PlusTest) {
 	calc->Enter(2);
 	calc->PressPlus();
 	calc->Enter(2);
 	calc->PressEquals();
 
 	if (calc->Display() != 4) {
-		// FAIL();
-		// 실패의 원인을 명시해야 한다.
 		FAIL() << "2+2 하였을 때 4가 아닙니다.";
 	} else {
 		SUCCEED();
