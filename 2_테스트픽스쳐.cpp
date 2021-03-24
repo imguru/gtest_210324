a67e5d1255b35744c25795d284afbe2785f14698
Chansik Yun <chansigi@gmail.com>
2021-03-24 05:10:33 +0000

    .

---
+++ 2_테스트픽스쳐.cpp
@@ -0,0 +1,60 @@
+// 2_테스트픽스쳐.cpp
+
+class Calculator {
+public:
+	double Display() { return 0; }
+
+	void Enter(double v) {}
+	void PressPlus() {}
+	void PressMinus() {}
+	void PressEquals() {}
+};
+
+//-------
+
+#include <gtest/gtest.h>
+
+#define SPEC printf
+
+TEST(CalculatorTest, Plus_2Plus2_Displays4) {
+	SPEC("2 더하기 2를 하였을 때 4가 나오는지 검증한다.\n");
+	Calculator* calc = new Calculator;
+
+	calc->Enter(2);
+	calc->PressPlus();
+	calc->Enter(2);
+	calc->PressEquals();
+
+	ASSERT_EQ(calc->Display(), 4) << "2+2 하였을 때";
+}
+
+TEST(CalculatorTest, PlusTest) {
+	Calculator* calc = new Calculator;
+
+	calc->Enter(2);
+	calc->PressPlus();
+	calc->Enter(2);
+	calc->PressEquals();
+
+	if (calc->Display() != 4) {
+		// FAIL();
+		// 실패의 원인을 명시해야 한다.
+		FAIL() << "2+2 하였을 때 4가 아닙니다.";
+	} else {
+		SUCCEED();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
