5976c3f1a99612e24654da677b54b029bbd04710
Chansik Yun <chansigi@gmail.com>
2021-03-24 02:30:15 +0000

    .

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 
 #include <gtest/gtest.h>
 
+// 정적 라이브러리 - libgtest.a => 권장
+//  - Archived
+//  - 프로그램 내부에 라이브러리가 복사되기 때문에, 라이브러리가 업데이트 되면 다시 컴파일을 수행해야 합니다.
+//  - 이식성이 뛰어나다.
+//
+// 동적 라이브러리 - libgtest.so
+//  - Shared Object
+//  - 프로세스가 수행될 때 라이브러리를 로드한다.
+//  - 라이브러리가 업데이트되어도, 기존 프로그램을 다시 컴파일할 필요가 없다.
+//  - 프로그램 내부에서 필요한 시점에 로드해서 사용할 수 있습니다.
+
 int main(int argc, char** argv) {
 	testing::InitGoogleTest(&argc, argv);
 
