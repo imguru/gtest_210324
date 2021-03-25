e57f035d66f5b6a7374ec200a97fc310c06d0ba7
Chansik Yun <chansigi@gmail.com>
2021-03-25 02:10:47 +0000

    .

--- 6_비기능테스트3.cpp
+++ 6_비기능테스트3.cpp
@@ -13,9 +13,12 @@
 //		  1) 소멸자 호출
 //		  2) Heap 메모리 해지 - operator delete
 //
+//  2) 테스트를 목적으로만 사용될 수 있어야 합니다.
+//     -> 조건부 컴파일을 사용하는 방법이 있습니다.
 
 class Image {
 public:
+#ifdef GTEST_LEAK_TEST
 	static int allocCount;
 
 	void* operator new(size_t size) {
@@ -27,22 +30,23 @@ public:
 		free(p);
 		--allocCount;
 	}
+#endif
 };
 
+#ifdef GTEST_LEAK_TEST
 int Image::allocCount = 0;
-
+#endif
 
 void DrawImage() {
 	Image* image1 = new Image;
-	new Image;
-	new Image;
-	new Image;
-	new Image;
-	new Image;
+	Image* image2 = new Image;
+	Image* image3 = new Image;
 
 	// ...
 
-	// delete image1;
+	delete image1;
+	// delete image2;
+	// delete image3;
 }
 
 class ImageTest : public testing::Test {
@@ -52,19 +56,21 @@ protected:
 	ImageTest() : alloc(0) {}
 
 	void SetUp() override {
+#ifdef GTEST_LEAK_TEST
 		alloc = Image::allocCount;
+#endif
 	}
 
 	void TearDown() override {
+#ifdef GTEST_LEAK_TEST
 		int diff = Image::allocCount - alloc;
 		EXPECT_EQ(diff, 0) << diff << " Object(s) leaks...";
+#endif
 	}
 };
 
 TEST_F(ImageTest, DrawImage) {
-	
 	DrawImage();
-
 }
 
 
--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+# g++ $1 -lgtest -L. -I. -pthread -DGTEST_LEAK_TEST
 g++ $1 -lgtest -L. -I. -pthread
