59dccde7c071160677961bf8c966ed9352e8a403
Chansik Yun <chansigi@gmail.com>
2021-03-25 05:50:33 +0000

    .

--- 10_테스트이벤트리스너.cpp
+++ 10_테스트이벤트리스너.cpp
@@ -2,9 +2,16 @@
 // 10_테스트이벤트리스너.cpp
 #include <gtest/gtest.h>
 
+using testing::UnitTest;
+using testing::TestSuite;
+using testing::TestInfo;
+using testing::TestPartResult;
+
 class MyTestPrinter : public testing::EmptyTestEventListener {
 public:
-	void OnTestProgramStart(const UnitTest& /*unit_test*/) override {}
+	void OnTestProgramStart(const UnitTest& /*unit_test*/) override {
+		printf(">> OnTestProgramStart\n");
+	}
 	void OnTestIterationStart(const UnitTest& /*unit_test*/, int /*iteration*/) override {}
 	void OnEnvironmentsSetUpStart(const UnitTest& /*unit_test*/) override {}
 	void OnEnvironmentsSetUpEnd(const UnitTest& /*unit_test*/) override {}
@@ -18,8 +25,22 @@ public:
 	void OnEnvironmentsTearDownStart(const UnitTest& /*unit_test*/) override {}
 	void OnEnvironmentsTearDownEnd(const UnitTest& /*unit_test*/) override {}
 	void OnTestIterationEnd(const UnitTest& /*unit_test*/, int /*iteration*/) override {}
-	void OnTestProgramEnd(const UnitTest& /*unit_test*/) override {}
+	
+	void OnTestProgramEnd(const UnitTest& /*unit_test*/) override {
+		printf(">> OnTestProgramEnd\n");
+	}
 };
+
+// main을 정의할 필요가 있습니다.
+int main(int argc, char** argv) {
+	testing::InitGoogleTest(&argc, argv);
+
+	testing::TestEventListeners& listeners = testing::UnitTest::GetInstance()->listeners();
+	listeners.Append(new MyTestPrinter);
+
+	return RUN_ALL_TESTS();
+}
+
 
 
 TEST(SampleTest, foo) {
