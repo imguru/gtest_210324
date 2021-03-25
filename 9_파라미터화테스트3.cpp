4b994b9b3782b32f60e7d11591ac4dcb4eba365a
Chansik Yun <chansigi@gmail.com>
2021-03-25 05:08:49 +0000

    .

--- 9_파라미터화테스트3.cpp
+++ 9_파라미터화테스트3.cpp
@@ -1,6 +1,27 @@
 
 #include <gtest/gtest.h>
 
+struct Input {
+	int arg1;
+	int arg2;
+
+	Input(int a, int b) : arg1(a), arg2(b) {}
+};
+
+class SampleTest : public testing::TestWithParam<Input> {
+};
+
+using testing::Values;
+INSTANTIATE_TEST_SUITE_P(InputValues, SampleTest, 
+	Values(Input(10, 20), Input(30, 40)));
+
+TEST_P(SampleTest, InputValues) {
+	Input input = GetParam();
+	printf("%d, %d\n", input.arg1, input.arg2);
+}
+
+
+#if 0
 // 입력 파라미터가 여러개인 경우 
 enum Color { BLACK, RED, WHITE };
 
@@ -23,6 +44,7 @@ TEST_P(AnimalTest, AnimalLooks) {
 
 	printf("%s(%d)\n", animal, color);
 }
+#endif
 
 
 
