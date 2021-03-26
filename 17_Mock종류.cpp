1df19367da80abad2c2ec4b0097ff47f73dcb53c
Chansik Yun <chansigi@gmail.com>
2021-03-26 05:17:49 +0000

    .

--- 17_Mock종류.cpp
+++ 17_Mock종류.cpp
@@ -24,16 +24,25 @@ void UseUser(User* p) {
 GMOCK WARNING:
 Uninteresting mock function call - returning directly.
     Function call: Go()
-NOTE: You can safely ignore the above warning unless this call should not happen.  Do not suppress it by blindly adding an EXPECT_CALL() if you don't mean to enforce the call.  See https://github.com/google/googletest/blob/master/googlemock/docs/cook_book.md#knowing-when-to-expect for details.
 #endif
 // 이유: Go에 대해서 MOCK_METHOD를 하였는데, 호출 여부를 검증하고 있지 않다.
 // 1. NaggyMock - 기본
 //   - 관심 없는 호출에 대해서 경고를 발생시킨다.
 //   - 테스트의 결과에는 영향이 없습니다.
 
+// 2. NiceMock
+//   - 경고를 발생시키지 않습니다.
+//
+// 3. StrictMock
+//   - 테스트가 실패합니다.
+
+using testing::NiceMock;
+using testing::StrictMock;
 
 TEST(UserTest, Say) {
-	MockUser mock;
+	// MockUser mock;
+	// NiceMock<MockUser> mock;
+	StrictMock<MockUser> mock;
 
 	EXPECT_CALL(mock, Say());
 	
