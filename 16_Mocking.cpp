1fa5bceb8431890e2b548b761fc3e49b3abd9b01
Chansik Yun <chansigi@gmail.com>
2021-03-26 04:07:38 +0000

    .

--- 16_Mocking.cpp
+++ 16_Mocking.cpp
@@ -11,6 +11,7 @@ struct Mp3 {
 	virtual void Foo() noexcept = 0;
 
 	virtual std::pair<bool, int> GetPair() const = 0;
+	virtual bool CheckMap(std::map<int, double> a, bool b) = 0;
 };
 
 // Mocking
@@ -23,10 +24,20 @@ struct Mp3 {
 //  - const: 필수
 //  - noxcept: 필수
 //  - stdcall: Windows platform(필수)
+
+// Type aliasing
+using BoolIntPair = std::pair<bool, int>;
+using IntDoubleMap = std::map<int, double>;
+
 class MockMp3 : public Mp3 {
 public:
 	// 템플릿인자가 두 개 이상인 경우, 괄호를 통해 묶어주어야 합니다.
-	MOCK_METHOD((std::pair<bool, int>), GetPair, (), (const, override));
+	// MOCK_METHOD(bool, CheckMap, ((std::map<int, double> a), bool b), (override));
+	// MOCK_METHOD((std::pair<bool, int>), GetPair, (), (const, override));
+	
+	// Type aliasing
+	MOCK_METHOD(bool, CheckMap, (IntDoubleMap a, bool b), (override));
+	MOCK_METHOD(BoolIntPair, GetPair, (), (const, override));
 
 	MOCK_METHOD(void, Play, (), (override));
 	MOCK_METHOD(void, Stop, (int n), (override));
